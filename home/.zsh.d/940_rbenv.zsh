5ecffa2892c0e517039e55afd61286f5df6a94cd
Sho Hashimoto <sho.hsmt@gmail.com>
2014-09-02 13:04:17 +0900

    zsh向けに設定を修正。

--- home/.zsh.d/940_rbenv.zsh
+++ home/.zsh.d/940_rbenv.zsh
@@ -1,9 +1,10 @@
 # rbenv を有効にする。
 # 以下に従って rbenv に関連するファイルが必要。
 # https://github.com/sstephenson/rbenv
+# http://qiita.com/hisaboh/items/52513515b94431a8e1e0
 
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+eval "$(rbenv init - zsh)"
 
 # Local variables:
 # mode: sh
