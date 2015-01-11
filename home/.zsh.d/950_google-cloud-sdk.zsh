bb94a68b39505a0f2126d623b2b174c294849347
Sho Hashimoto <sho.hsmt@gmail.com>
2015-01-11 15:31:16 +0900

    追加。

---
+++ home/.zsh.d/950_google-cloud-sdk.zsh
@@ -0,0 +1,12 @@
+# Google Cloud SDKを有効にする。
+
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/sho-h/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables bash completion for gcloud.
+source '/Users/sho-h/google-cloud-sdk/completion.zsh.inc'
+
+# Local variables:
+# mode: sh
+# coding: utf-8
+# End:
