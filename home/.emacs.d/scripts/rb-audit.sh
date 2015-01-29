a86fd1498f42d4eae98c3ec8545e2c559dc4ea05
Sho Hashimoto <sho.hsmt@gmail.com>
2015-01-29 13:52:36 +0900

    非RailsプロジェクトでRakefileがない or rake aboutがない場合の標準エラーの出力を表示されないように修正。

--- home/.emacs.d/scripts/rb-audit.sh
+++ home/.emacs.d/scripts/rb-audit.sh
@@ -8,7 +8,7 @@ ruby -w -c $1
 rc_exitcode=$?
 
 # Brakeman vulnerability check
-rails_root=`bundle exec rake about | grep "Application root" | awk '{print $3}'`
+rails_root=`bundle exec rake about 2>/dev/null | grep "Application root" | awk '{print $3}'`
 if [ "x${rails_root}" != "x" ]; then
   bundle exec brakeman ${rails_root} -f json -z | ruby -r json -e 'JSON.parse(STDIN.read)["warnings"].collect {|h| path = h["file"].gsub(File.dirname(h["file"]), File.dirname(ARGV[0])); [path, h["line"], h["warning_type"], h["message"]].join(":")}.each {|s| STDERR.puts s}' $1
   exit $? || $rc_exitcode
