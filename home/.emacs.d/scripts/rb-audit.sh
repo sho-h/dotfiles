a970bb02d5eec84025800b7fff1701e11db72ff8
Sho Hashimoto <sho.hsmt@gmail.com>
2015-01-29 22:54:19 +0900

    該当のファイルのみチェックするように修正。

--- home/.emacs.d/scripts/rb-audit.sh
+++ home/.emacs.d/scripts/rb-audit.sh
@@ -10,7 +10,7 @@ rc_exitcode=$?
 # Brakeman vulnerability check
 rails_root=`bundle exec rake about 2>/dev/null | grep "Application root" | awk '{print $3}'`
 if [ "x${rails_root}" != "x" ]; then
-  bundle exec brakeman ${rails_root} -f json -z | ruby -r json -e 'JSON.parse(STDIN.read)["warnings"].collect {|h| path = h["file"].gsub(File.dirname(h["file"]), File.dirname(ARGV[0])); [path, h["line"], h["warning_type"], h["message"]].join(":")}.each {|s| STDERR.puts s}' $1
+  bundle exec brakeman ${rails_root} -f json -z --only-files $1 | ruby -r json -e 'JSON.parse(STDIN.read)["warnings"].collect {|h| path = h["file"].gsub(File.dirname(h["file"]), File.dirname(ARGV[0])); [path, h["line"], h["warning_type"], h["message"]].join(":")}.each {|s| STDERR.puts s}' $1
   exit $? || $rc_exitcode
 fi
 
